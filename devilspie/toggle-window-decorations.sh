b7394aee9d7ff1e65e4452ad2d0b8cbfbefa07bb
michja <30815899+michja@users.noreply.github.com>
2017-09-11 15:34:07 +0100

    Create toggle-window-decorations.sh

---
+++ devilspie/toggle-window-decorations.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+DIRECTORY='/home/michael/.devilspie/'
+ONFILE='all-deco-on.dsx'
+OFFFILE='all-deco-off.dsx'
+ACTIVEFILE='deco.ds'
+
+CURRENT="$(readlink $DIRECTORY$ACTIVEFILE)"
+ rm -f "$DIRECTORY$ACTIVEFILE"
+if [ "$CURRENT" == "$DIRECTORY$ONFILE" ]; then
+	ln -s "$DIRECTORY$OFFFILE" "$DIRECTORY$ACTIVEFILE"
+	echo "turn off"
+else
+	echo "turn on"
+	#echo "$CURRENT"
+	#echo "$ONFILE"
+	ln -s "$DIRECTORY$ONFILE" "$DIRECTORY$ACTIVEFILE"
+fi
+
+pkill devilspie; devilspie -a &
